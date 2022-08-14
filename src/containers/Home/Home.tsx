7000049627235c9ff9fd3e9ff861b0d3ff073cd1
Albert <albert@diskus.studio>
2022-08-14 16:13:52 +0200

    Center content

--- src/containers/Home/Home.tsx
+++ src/containers/Home/Home.tsx
@@ -1,22 +1,32 @@
 import image from '@assets/me.png'
 import { exampleValue } from '@constants'
-import { m } from 'framer-motion'
 
 import styles from './Home.module.scss'
 
 // Destructure the styles.
 const { heroImage, article, header, subheader } = styles
 
+// Animation
+const container = {
+    hidden: { opacity: 0 },
+    show: {
+        opacity: 1,
+        transition: {
+            delayChildren: 0.5,
+        },
+    },
+}
+
 function Home(): JSX.Element {
     return (
         <article className={article}>
             <img className={heroImage} src={image} />
-            <m.h1 layout className={header}>
+            <h1 className={header}>
                 Welcome to the {import.meta.env.VITE_PROJECT_NAME}!
-            </m.h1>
-            <m.h2 layout className={subheader}>
+            </h1>
+            <h2 className={subheader}>
                 Grab a {exampleValue}, and start building.
-            </m.h2>
+            </h2>
         </article>
     )
 }
